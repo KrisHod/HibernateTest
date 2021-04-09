dbb1309027746e09745ba3cad1e09f7b16ccc17b
Kris <krisgodom@gmail.com>
2021-04-09 16:44:14 -0300

    overrided methods

--- src/main/java/com/example/hibernate/repository/MerchantRepository.java
+++ src/main/java/com/example/hibernate/repository/MerchantRepository.java
@@ -1,10 +1,13 @@
 package com.example.hibernate.repository;
 
 import com.example.hibernate.entity.Merchant;
+import com.example.hibernate.entity.Result;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Repository
 public class MerchantRepository implements IMerchantRepository {
@@ -19,5 +22,19 @@ public class MerchantRepository implements IMerchantRepository {
     @Override
     public void save(Merchant merchant) {
         em.persist(merchant);
+    }
+
+    @Override
+    public List<Merchant> getSortedByNeedToPay() {
+        String txt = "SELECT m FROM merchant m ORDER BY m.needToSend";
+        TypedQuery<Merchant> query = em.createQuery(txt, Merchant.class);
+        return query.getResultList();
+    }
+
+    public List<Result> getTotalReport() {
+        String txt = "SELECT new com.example.hibernate.entity.Result (p.merchant.name, SUM(p.chargePaid), count(p))";
+        txt += "FROM payment p GROUP BY p.merchant.name";
+        TypedQuery<Result> query = em.createQuery(txt, Result.class);
+        return query.getResultList();
     }
 }
