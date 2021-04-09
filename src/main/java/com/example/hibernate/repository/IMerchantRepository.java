66525807c479996bf30c13b4cfb463ad9021a2ce
Kris <krisgodom@gmail.com>
2021-04-09 16:43:44 -0300

    added methods

--- src/main/java/com/example/hibernate/repository/IMerchantRepository.java
+++ src/main/java/com/example/hibernate/repository/IMerchantRepository.java
@@ -2,7 +2,9 @@ package com.example.hibernate.repository;
 
 import com.example.hibernate.entity.Merchant;
 
+import java.util.List;
+
 public interface IMerchantRepository {
     public Merchant findById(int id);
     public void save(Merchant merchant);
-}
+    public List<Merchant> getSortedByNeedToPay();}
