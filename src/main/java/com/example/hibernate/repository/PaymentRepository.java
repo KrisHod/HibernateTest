101d6e8fa9b011d067c6e1c42cd31987155cb8fa
Kris <krisgodom@gmail.com>
2021-04-09 16:45:00 -0300

    overrided methods

--- src/main/java/com/example/hibernate/repository/PaymentRepository.java
+++ src/main/java/com/example/hibernate/repository/PaymentRepository.java
@@ -1,2 +1,19 @@
-package com.example.hibernate.repository;public class PaymentRepository {
+package com.example.hibernate.repository;
+
+import com.example.hibernate.entity.Customer;
+import com.example.hibernate.entity.Payment;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class PaymentRepository implements IPaymentRepository{
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Payment findById(int id) {
+        return em.find(Payment.class, id);
+    }
 }
