cc676d189eb91befe09635212e26228fe64a4658
Kris <krisgodom@gmail.com>
2021-04-09 16:44:39 -0300

    added methods

--- src/main/java/com/example/hibernate/repository/IPaymentRepository.java
+++ src/main/java/com/example/hibernate/repository/IPaymentRepository.java
@@ -1,2 +1,7 @@
-package com.example.hibernate.repository;public class IPaymentRepository {
+package com.example.hibernate.repository;
+
+import com.example.hibernate.entity.Payment;
+
+public interface IPaymentRepository {
+    public Payment findById(int id);
 }
